019f82e8d3511c41c150ed93c84b3fe951a21dab
Alex Grigore <agworksbox@gmail.com>
2026-06-21 20:36:26 +0100

    init

---
+++ CodeBreaker/CodeBreaker/CodeBreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBreakerApp.swift
+//  CodeBreaker
+//
+//  Created by Tatiana Kornilova on 29/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct CodeBreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodeBreaker/CodeBreaker/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CodeBreaker
+//
+//  Created by Tatiana Kornilova on 29/01/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack{
+            Image(systemName: "globe")
+            if true {
+                Text("greetings!")
+            }
+            Text("howdy!")
+            Circle()
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
